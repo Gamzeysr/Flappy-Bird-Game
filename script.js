237ed74a0ac321d7439df6bfacf1de375af09612
Gamze <gamzeyasar35@icloud.com>
2023-02-11 13:19:22 +0300

    keydown enter

---
+++ script.js
@@ -0,0 +1,31 @@
+let move_speed = 3, grativy = 0.5;
+let bird = document.querySelector('.bird');
+let img = document.getElementById('bird-1');
+
+// getting bird element properties
+let bird_props = bird.getBoundingClientRect();
+
+let background = document.querySelector('background').getBoundingClientRect();
+let score_val = document.querySelector('.score_val');
+let message = document.querySelector('.message');
+let score_title = document.querySelector('.score_title');
+
+let game_state = 'Start';
+img.style.display = 'none';
+message.classList.add('messageStyle');
+
+document.addEventListener('keydown', (e) => {
+    if (e.key == 'Enter' && game_state != 'Play') {
+        document.querySelectorAll('.pipe_sprite').forEact((e) => {
+            e.remove();
+        });
+        img.style.display = 'block';
+        bird.style.top = '40vh';
+        game_state = 'Play';
+        message.innerHTML = '';
+        score_title.innerHTML = 'Score : ';
+        score_val.innerHTML = '0';
+        message.classList.remove('messageStyle');
+        play();
+    }
+})
